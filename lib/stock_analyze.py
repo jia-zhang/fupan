4264d24ebc6ef9af887ad6b98f9ea981214d111f
cookiett <jia_zhang@trendmicro.com.cn>
2018-10-20 19:12:16 +0800

    update

--- lib/stock_analyze.py
+++ lib/stock_analyze.py
@@ -1,26 +1,50 @@
 import requests
 import re
 import time
+import os
 from stock_query import StockQuery
 
 class StockAnalyze():
     def __init__(self):
         pass
+
+    def get_delta(self,stock_id,day_num):
+        file_name = "./data/%s.json"%(stock_id)
+        with open(file_name,'r') as f:
+                output = f.read()
+        stock_detail = eval(output)
+        price_open = float(stock_detail[-1-day_num]['close'])
+        price_close = float(stock_detail[-1]['close'])
+        lift_status = (price_close-price_open)*100/price_open
+        return lift_status
     
-    def get_lift_high(self,time_range,top_n):
+    def get_volume(self,stock_id,day_num):
+        q = StockQuery()
+        gb = q.get_stock_basic(stock_id,"test")
+        print(gb)
+        file_name = "./data/%s.json"%(stock_id)
+        with open(file_name,'r') as f:
+                output = f.read()
+        stock_detail = eval(output)
+        volume = int(stock_detail[-1-day_num]['volume'])
+        return volume/float(gb)
+    
+    def get_lift_high(self,top_n):
         q = StockQuery()
         last_trading_date = q.get_last_trading_date()
-        s_list = q.get_stock_list_from_file('stocks.csv')
+        #s_list = q.get_stock_list_from_file('stocks.csv')
         status = []
         i=0
-        for s in s_list:
-            stock_detail = eval(q.get_stock_detail(s,240,time_range))
+        for file_name in os.listdir('./data'):
+            with open("./data/%s"%(file_name),'r') as f:
+                output = f.read()
+            stock_detail = eval(output)
             price_open = float(stock_detail[0]['open'])
             price_close = float(stock_detail[-1]['close'])
             lift_status = (price_close-price_open)*100/price_open
             #today = str(datetime.date.today())
             if(stock_detail[-1]['day']==last_trading_date):
-                status.append("%s:%s"%(s,lift_status))
+                status.append("%s:%s"%(file_name.replace('.json',''),lift_status))
             i=i+1
             #time.sleep(1)
             if i==100:
@@ -30,4 +54,5 @@ class StockAnalyze():
 
 if __name__ == '__main__':
     a = StockAnalyze()
-    a.get_lift_high(10,10)
+    #print(a.get_delta('sz000002',9))
+    print(a.get_volume('sz000002',4))
--- lib/stock_query.py
+++ lib/stock_query.py
@@ -1,13 +1,15 @@
 import requests
 import re
+import time
+import os
 
 class StockQuery():
     def __init__(self):
-        self.stock_list_url = "http://quote.eastmoney.com/stocklist.html"
+        #self.stock_list_url = "http://quote.eastmoney.com/stocklist.html"
         pass
     
     def get_stock_list(self):
-        resp = requests.get(self.stock_list_url)
+        resp = requests.get("http://quote.eastmoney.com/stocklist.html")
         resp.encoding = 'gb2312'
         s = r'<li><a target="_blank" href="http://quote.eastmoney.com/(.*?).html">'
         pat = re.compile(s)
@@ -29,6 +31,7 @@ class StockQuery():
         return output.split(',')
     
     def get_stock_detail(self,stock_id,time_range,count):
+        #headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'}
         detail_url = ("http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?"
                     "symbol=%s&scale=%s&ma=no&datalen=%s"
         )%(stock_id,time_range,count)
@@ -36,16 +39,78 @@ class StockQuery():
         return resp.text.replace('day','"day"').replace('open','"open"').replace('low','"low"').\
         replace('high','"high"').replace('close','"close"').replace('volume','"volume"')
     
+    def get_stock_basic(self,stock_id,item):
+        #resp = requests.get("http://finance.sina.com.cn/realstock/company/%s/nc.shtml"%(stock_id))
+        headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'}
+        resp = requests.get("https://xueqiu.com/S/%s"%(stock_id),headers=headers)
+        resp.encoding = 'gb2312'
+        #s = r'var currcapital = (.*?);'
+        s = r'"float_shares":(\d*?),'
+        pat = re.compile(s)
+        codes = pat.findall(resp.text)
+        return codes[0]
+
     def get_last_trading_date(self):
+        #headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'}
         detail_url = "http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=sh000001&scale=240&ma=no&datalen=5"
         resp = requests.get(detail_url)
         return eval(resp.text.replace('day','"day"').replace('open','"open"').replace('low','"low"').\
         replace('high','"high"').replace('close','"close"').replace('volume','"volume"'))[-1]['day']
+    
+    def dump_stock_dynamic(self):
+        s_list = self.get_stock_list_from_file('stocks.csv')
+        i=0
+        for s in s_list:
+            print("Dumping stock dynamic %s..."%(s))
+            if (os.path.exists("./data/%s.json"%(s))):
+                print("%s already exists, skip"%(s))
+                continue
+            stock_detail = self.get_stock_detail(s,240,10)
+            i=i+1
+            with open("./data/%s.json"%(s),'w') as f:
+                f.write(stock_detail)
+            if (i%10==0):
+                time.sleep(1)
+    
+    def dump_stock_static(self):
+        s_list = self.get_stock_list_from_file('stocks.csv')
+        headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'}
+        i=0
+        f = open("stock_static.json",'w')
+        #f.write('{')
+        master_dict = {}
+        for s in s_list:
+            print("Dumping stock static %s..."%(s))
+            resp = requests.get("https://xueqiu.com/S/%s"%(s),headers=headers)
+            if (resp.status_code==404):
+                print("Get code 404 on stock %s"%(s))
+                continue
+            resp.encoding = 'utf-8'
+            float_shares = re.compile(r'"float_shares":(\d*?),').findall(resp.text)[0]
+            stock_name = re.compile(r'"name":(.*?),').findall(resp.text)[0]      
+            market_capital = re.compile(r'"market_capital":(.*?),').findall(resp.text)[0]             
+            stock_dict = {}
+            stock_dict['float_shares'] = str(float_shares)  
+            stock_dict['stock_name'] = str(stock_name)
+            stock_dict['stock_name'] = str(stock_name)
+            master_dict[s] = stock_dict
+            #if i==0:
+            #    f.write("%s"%(str(master_dict)))
+            #else:
+            #    f.write(",%s"%(str(master_dict)))
+            i = i+1
+            if i==3:
+                break
+        f.write(str(master_dict))
+        #f.write('}')
+        f.close()
 
     
 
 if __name__ == '__main__':
     t = StockQuery()
+    t.dump_stock_static()
     #t.save_stock_list("stocks.csv")
-    print(t.get_last_trading_date())
+    #print(t.get_last_trading_date())
+    #print(t.get_stock_detail('sz000007',240,10))
 
