75820b806aa104615d1b2845c016e233e244cbf3
jia_zhang@trendmicro.com.cn <jia_zhang@trendmicro.com.cn>
2018-10-21 14:28:51 +0800

    rename file

--- lib/stock_tag.py
+++ lib/stock_tag.py
@@ -4,6 +4,28 @@ class StockTag():
     def __init__(self):
         pass
     
+    def add_propery(self,stock_id,property_dict):
+        file_name = "./data/%s.static.json"%(stock_id)
+        f = open(file_name,'r',encoding='utf-8')
+        info = json.load(f)
+        f.close()
+        for key in property_dict.keys():
+            info[stock_id][key] = property_dict[key]
+        print(info)
+        with open(file_name,'w') as f:
+            f.write(json.dumps(info))
+    
+    def remove_property(self,stock_id,property_key):
+        file_name = "./data/%s.static.json"%(stock_id)
+        f = open(file_name,'r',encoding='utf-8')
+        info = json.load(f)
+        f.close()
+        info[stock_id].pop(property_key)
+        print(info)
+        with open(file_name,'w') as f:
+            f.write(json.dumps(info))
+
+
     def add_tag(self,stock_id,new_tag):
         file_name = "./data/%s.static.json"%(stock_id)
         f = open(file_name,'r',encoding='utf-8')
@@ -34,4 +56,6 @@ class StockTag():
 if __name__ == '__main__':
     t = StockTag()
     #t.add_tag('sz000002', '稀土')
-    print(t.get_tag('sz000002'))
+    #print(t.get_tag('sz000002'))
+    #t.add_propery('sh600000',{"tag1":{"ddd":"ccc"}})
+    t.remove_property('sh600000','tag1')
